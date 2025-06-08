fa9f758fdeeb3f51a1e051a6de4190d66c79b940
Carlos Borges <carloos-boorges@hotmail.com>
2025-06-07 23:20:55 -0300

    feat: cria MovieRepository estendendo JpaRepository para operações básicas de filmes

---
+++ src/main/java/com/carlosborges/movieflix/repository/MovieRepository.java
@@ -0,0 +1,8 @@
+package com.carlosborges.movieflix.repository;
+import com.carlosborges.movieflix.entity.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository  extends JpaRepository<Movie, Long> {
+}
