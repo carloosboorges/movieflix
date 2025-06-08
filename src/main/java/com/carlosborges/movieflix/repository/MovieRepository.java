993c14bf9b9d605e26328aec6507e0a25f3fd169
Carlos Borges <carloos-boorges@hotmail.com>
2025-06-08 17:33:48 -0300

    feat: adiciona método findByCategories em MovieRepository

--- src/main/java/com/carlosborges/movieflix/repository/MovieRepository.java
+++ src/main/java/com/carlosborges/movieflix/repository/MovieRepository.java
@@ -1,8 +1,14 @@
 package com.carlosborges.movieflix.repository;
+import com.carlosborges.movieflix.entity.Category;
 import com.carlosborges.movieflix.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MovieRepository  extends JpaRepository<Movie, Long> {
+
+    List<Movie> findByCategories(List<Category> categories);
+
 }
