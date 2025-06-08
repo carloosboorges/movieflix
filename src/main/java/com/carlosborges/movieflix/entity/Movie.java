66477154108489c238577f3187f706dad8b7d5ae
Carlos Borges <carloos-boorges@hotmail.com>
2025-06-07 23:19:12 -0300

    feat: adiciona entidade Movie para representar os filmes no banco de dados

---
+++ src/main/java/com/carlosborges/movieflix/entity/Movie.java
@@ -0,0 +1,43 @@
+package com.carlosborges.movieflix.entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "Movie")
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", length = 100, nullable = false)
+    private String title;
+
+    private String description;
+
+    @Column(name = "release_date")
+    private LocalDate releaseDate;
+
+    private double rating;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+}
