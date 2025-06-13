22905b1d69cda261517573453a9f573d40477433
Carlos Borges <carloos-boorges@hotmail.com>
2025-06-13 12:48:35 -0300

    Inclui @Valid para validação nas requisições do StreamingController

--- src/main/java/com/carlosborges/movieflix/controller/StreamingController.java
+++ src/main/java/com/carlosborges/movieflix/controller/StreamingController.java
@@ -5,6 +5,7 @@ import com.carlosborges.movieflix.controller.response.StreamingResponse;
 import com.carlosborges.movieflix.entity.Streaming;
 import com.carlosborges.movieflix.mapper.StreamingMapper;
 import com.carlosborges.movieflix.service.StreamingService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -30,7 +31,7 @@ public class StreamingController {
     }
 
     @PostMapping
-    public ResponseEntity<StreamingResponse> saveStreaming(@RequestBody StreamingRequest request) {
+    public ResponseEntity<StreamingResponse> saveStreaming(@Valid @RequestBody StreamingRequest request) {
         Streaming newStreaming = StreamingMapper.toStreaming(request);
         Streaming savedStreaming = streamingService.saveStreaming(newStreaming);
         return ResponseEntity.status(HttpStatus.CREATED).body(StreamingMapper.toStreamingResponse(savedStreaming));
